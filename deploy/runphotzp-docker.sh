dc97f53487775aa8a36bb6a0eb9b369e50babc86
Daniel Harbeck <drharbeck@gmail.com>
2018-08-02 11:21:36 -0700

    more minor changes in runphotzp-docker shell script: time stamp in log when started to validate that cron did call, and parallelizing plot generation.

--- deploy/runphotzp-docker.sh
+++ deploy/runphotzp-docker.sh
@@ -2,14 +2,14 @@
 #TODO:
 # 1. command line paramters as docker defined ENV variables
 
+echo "Hello photzp `date`"
 cd /lco/throughput/src/longtermphotzp
 
 # TODO: populate via DOCKER
 sqlfile="/database/lcophotzp.db"
 
-printf %s\\n {fl,fs,kb} |  xargs -n 1 -P 3 python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype
+printf %s\\n {fl,fs,kb} |  xargs -n 1 -P 3 /usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype
 
-python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter rp
-python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter gp
+printf %s\\ {rp,gp} | xargs -n 1 -P 2 /usr/local/bin/python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter
 
 
