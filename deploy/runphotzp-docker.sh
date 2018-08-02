bc8e6d89e10cb2593148ac1f2415d3723be611ab
Daniel Harbeck <drharbeck@gmail.com>
2018-08-02 11:46:08 -0700

    missin \\n in runphotzp-docker script  corrected.

--- deploy/runphotzp-docker.sh
+++ deploy/runphotzp-docker.sh
@@ -10,6 +10,6 @@ sqlfile="/database/lcophotzp.db"
 
 printf %s\\n {fl,fs,kb} |  xargs -n 1 -P 3 /usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype
 
-printf %s\\ {rp,gp} | xargs -n 1 -P 2 /usr/local/bin/python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter
+printf %s\\n {rp,gp} | xargs -n 1 -P 2 /usr/local/bin/python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter
 
 
