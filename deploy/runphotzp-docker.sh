5bdc91627464a2252ebc0327826da7b6f5feef52
Daniel Harbeck <drharbeck@gmail.com>
2018-10-10 13:46:46 -0700

    serialize execution of tasks, no parallel computing.

--- deploy/runphotzp-docker.sh
+++ deploy/runphotzp-docker.sh
@@ -8,8 +8,12 @@ cd /lco/throughput/src/longtermphotzp
 # TODO: populate via DOCKER
 sqlfile="/database/lcophotzp.db"
 
-printf %s\\n {fl,fa,fs,kb} |  xargs -n 1 -P 3 /usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype
+#printf %s\\n {fl,fa,fs,kb} |  xargs -n 1 -P 3 /usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype
 
-printf %s\\n {rp,gp} | xargs -n 1 -P 2 /usr/local/bin/python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter
+/usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype fl
+/usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype fa
+/usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype fs
+/usr/local/bin/python3 photcalibration.py --photodb $sqlfile --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype kb
 
-
+/usr/local/bin/python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter gp
+/usr/local/bin/python3 longtermphotzp.py --database $sqlfile --outputdirectory /database --filter rp
