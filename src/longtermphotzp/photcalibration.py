651e0e9282b0803191ed2fb2486e6d30482cc3e4
Daniel Harbeck <drharbeck@gmail.com>
2018-12-17 14:26:54 -0800

    draft implementaion on refcat2 sqlite query

--- src/longtermphotzp/photcalibration.py
+++ src/longtermphotzp/photcalibration.py
@@ -17,9 +17,12 @@ import sys
 import logging
 from astropy.io import fits
 from astropy.wcs import WCS
+from astropy.table import Table
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 import datetime
+import sqlite3
+
 
 _logger = logging.getLogger(__name__)
 
@@ -36,7 +39,7 @@ class PhotCalib():
     def __init__(self, ps1dir):
         # super(PhotCalib, self).__init__(pipeline_context)
         # TODO: capture error if no ps1 catalog is found at location
-        self.ps1catalog = PS1IPP(ps1dir)
+        self.ps1catalog = refcat2(ps1dir)
 
     def do_stage(self, images):
         """ future hook for BANZAI pipeline integration
@@ -280,6 +283,91 @@ class PhotCalib():
         #     f.close()
 
         return photzp
+
+class refcat2:
+
+    FILTERMAPPING = {}
+    FILTERMAPPING['gp'] = {'refMag': 'g', 'colorTerm': 0.0, 'airmassTerm': 0.20, 'defaultZP': 0.0}
+    FILTERMAPPING['rp'] = {'refMag': 'r', 'colorTerm': 0.0, 'airmassTerm': 0.12, 'defaultZP': 0.0}
+    FILTERMAPPING['ip'] = {'refMag': 'i', 'colorTerm': 0.0, 'airmassTerm': 0.08, 'defaultZP': 0.0}
+    FILTERMAPPING['zp'] = {'refMag': 'z', 'colorTerm': 0.0, 'airmassTerm': 0.05, 'defaultZP': 0.0}
+
+    ###  PS to SDSS color transformations according to  Finkbeiner 2016
+    ###  http://iopscience.iop.org/article/10.3847/0004-637X/822/2/66/meta#apj522061s2-4 Table 2
+    ###  Note that this transformation is valid for stars only. For the purpose of photometric
+    ###  calibration, it is desirable to select point sources only from the input catalog.
+
+    ## Why reverse the order of the color term entries? Data are entered in the order as they are
+    ## shown in paper. Reverse after the fact to avoid confusion when looking at paper
+
+    ps1colorterms = {}
+    ps1colorterms['g'] = [-0.01808, -0.13595, +0.01941, -0.00183][::-1]
+    ps1colorterms['r'] = [-0.01836, -0.03577, +0.02612, -0.00558][::-1]
+    ps1colorterms['i'] = [+0.01170, -0.00400, +0.00066, -0.00058][::-1]
+    ps1colorterms['z'] = [-0.01062, +0.07529, -0.03592, +0.00890][::-1]
+
+
+    def __init__(self, dbfile):
+        if (dbfile is None) or (not os.path.isfile(dbfile)):
+            _logger.error("Unable to find reference catalog: %s" % (str(dbfile)))
+            self.dbfile = None
+            return
+        self.dbfile = dbfile
+
+    def isInCatalogFootprint(self, ra, dec):
+        return True
+
+
+    def PS1toSDSS(self, table):
+        """
+        Modify table in situ from PS1 to SDSS, requires column names compatible with ps1colorterms definition.
+
+        :param table:
+        :return: modified table.
+        """
+        if table is not None:
+            pscolor = table['g'] - table['i']
+            for filter in self.ps1colorterms:
+                colorcorrection = np.polyval(self.ps1colorterms[filter], pscolor)
+                table[filter] -= colorcorrection
+
+        return table
+
+
+    def get_reference_catalog(self, ra, dec, radius, overwrite_select=False):
+
+        rows = None
+        try:
+            connection = sqlite3.connect(self.dbfile)
+            cursor = connection.cursor()
+            if (not radius == None and radius > 0):
+                min_dec = dec - radius
+                max_dec = dec + radius
+                min_ra = ra - radius / math.cos(math.radians(dec))
+                max_ra = ra + radius / math.cos(math.radians(dec))
+
+
+            sql_command = 'select sources.RA, sources.Dec, sources.g,sources.r,sources.i, sources.z from sources, positions ' \
+                      'where positions.ramin >= {ramin} and positions.ramax <= {ramax} ' \
+                      'and positions.decmin >= {decmin} and positions.decmax <= {decmax} ' \
+                      'and positions.objid = sources.objid'
+
+
+
+            sql_command = sql_command.format(ramin=min_ra, ramax=max_ra, decmin=min_dec, decmax=max_dec)
+            _logger.info (sql_command)
+            cursor.execute(sql_command)
+            rows = cursor.fetchall()
+            print (rows)
+            table = Table (rows, names=['ra','dec','g','r','i','z'])
+        except:
+            _logger.exception("While trying to read from database:")
+            exit(1)
+
+        table = self.PS1toSDSS(table)
+
+        return table
+
 
 
 class PS1IPP:
@@ -560,7 +648,7 @@ def parseCommandLine():
 
     parser.add_argument('--log-level', dest='log_level', default='INFO', choices=['DEBUG', 'INFO'],
                         help='Set the log level')
-    parser.add_argument('--ps1dir', dest='ps1dir', default='~/Catalogs/ps1odi/panstarrs/',
+    parser.add_argument('--ps1dir', dest='ps1dir', default='~/Catalogs/refcat2/refcat2.db',
                         help='Directory of PS1 catalog')
     parser.add_argument("--diagnosticplotsdir", dest='outputimageRootDir', default=None,
                         help='Output directory for diagnostic photometry plots. No plots generated if option is omitted. This is a time consuming task. ')
