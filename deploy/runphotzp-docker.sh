98f45716e3fdfa173d56dafc13d53ca82597e571
Daniel Harbeck <drharbeck@gmail.com>
2020-02-05 10:32:40 -0800

    make docker isntance use the aws download option to fetch images.

--- deploy/runphotzp-docker.sh
+++ deploy/runphotzp-docker.sh
@@ -2,9 +2,9 @@
 
 #printf %s\\n {fl,fa,fs,kb} |  xargs -n 1 -P 3 /usr/local/bin/python3 photcalibration.py --photodb $LONGTERMPHOTZP_DATABASE_FILE --ps1dir /panstarrs --mintexp 10 --lastNdays 3 --cameratype
 
-photcalibration --photodb $LONGTERMPHOTZP_DATABASE_FILE --refcat2db $REFCAT2_DATABASE_FILE --mintexp 10 --lastNdays 3 --cameratype fa
-photcalibration --photodb $LONGTERMPHOTZP_DATABASE_FILE --refcat2db $REFCAT2_DATABASE_FILE --mintexp 10 --lastNdays 3 --cameratype fs
-photcalibration --photodb $LONGTERMPHOTZP_DATABASE_FILE --refcat2db $REFCAT2_DATABASE_FILE --mintexp 10 --lastNdays 3 --cameratype kb
+photcalibration --useaws --photodb $LONGTERMPHOTZP_DATABASE_FILE --refcat2db $REFCAT2_DATABASE_FILE --mintexp 10 --lastNdays 3 --cameratype fa
+photcalibration --useaws --photodb $LONGTERMPHOTZP_DATABASE_FILE --refcat2db $REFCAT2_DATABASE_FILE --mintexp 10 --lastNdays 3 --cameratype fs
+photcalibration --useaws --photodb $LONGTERMPHOTZP_DATABASE_FILE --refcat2db $REFCAT2_DATABASE_FILE --mintexp 10 --lastNdays 3 --cameratype kb
 
 longtermphotzp --database $LONGTERMPHOTZP_DATABASE_FILE --outputdirectory /database --filter gp
 longtermphotzp --database $LONGTERMPHOTZP_DATABASE_FILE --outputdirectory /database --filter rp
