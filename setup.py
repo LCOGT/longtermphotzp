4efe7b99c17a1a44f8dcd8eb9206fae43b1fadec
Daniel Harbeck <drharbeck@gmail.com>
2020-02-07 09:37:09 -0800

    version bump to 6.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'longtermphotzp',
-    version="6.4",
+    version="6.5",
     author='Daniel Harbeck',
     author_email='dharbeck@lco.global',
     classifiers=[
