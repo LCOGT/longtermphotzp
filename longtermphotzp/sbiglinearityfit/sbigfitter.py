1a6af7538162fef122d1228b790dc16c871d757f
Daniel Harbeck <drharbeck@gmail.com>
2021-12-08 18:52:17 -0800

    experimenatal compensation of sbig linearity and bootstrap fitting

--- longtermphotzp/sbiglinearityfit/sbigfitter.py
+++ longtermphotzp/sbiglinearityfit/sbigfitter.py
@@ -1,5 +1,8 @@
 import datetime
 import logging
+import os.path
+
+import sep
 
 import scipy
 from astropy.io import fits
@@ -24,7 +27,7 @@ def redo_phot_on_matched_catalog(imagedata, matchedcatalog, apertures):
     matchedcatalog['instmag'] = -2.5 * np.log10(matchedcatalog['FLUX'] / matchedcatalog['exptime'])
 
 
-def fitmerritfunction(x, imageobject, matchedcatlog):
+def fitmerritfunction(x, imageobject, matchedcatlog, fwhm, pngname=None):
     ''' x: vector of paramters
         inputimage: pixel array
         inputcatlog: list of sources where to to photometry.
@@ -35,18 +38,21 @@ def fitmerritfunction(x, imageobject, matchedcatlog):
     zk = z**k
     assert (z>=0)
     assert (k>=0.5)
-    rectifieddata = imageobject['SCI'].data
+    rectifieddata = imageobject['SCI'].data.astype(float)
     rectifieddata[rectifieddata<0] = 0
     rectifieddata = (rectifieddata  + z) ** k - zk
     rectifieddata[rectifieddata < 0] = 0
     rectifieddata = rectifieddata ** (1 / k)
 
-    redo_phot_on_matched_catalog(rectifieddata, matchedcatlog, [9, 10, 15])
+
+    redo_phot_on_matched_catalog(rectifieddata, matchedcatlog, [9,10,15])
+    #redo_phot_on_matched_catalog(rectifieddata, matchedcatlog, [12,15,20])
 
     brightest = np.min(matchedcatlog['refmag'])
-    nbrightest = matchedcatlog['refmag'] - brightest < 4
+
     zp = matchedcatlog['refmag'] - matchedcatlog['instmag']
-    myzp = np.nanmean(zp[nbrightest])
+    nbrightest = (matchedcatlog['refmag'] - brightest < 4)# & (np.abs (zp) - np.nanmedian (zp) < 1) & (matchedcatlog['refmag'] > 12)
+    myzp = np.nanmedian(zp[nbrightest])
     deviation = np.nanstd (zp[nbrightest]-myzp)
 
     selzp = zp[nbrightest]
@@ -55,25 +61,28 @@ def fitmerritfunction(x, imageobject, matchedcatlog):
     f=np.polyfit (selref,selzp,1)
     p = np.poly1d(f)
     delta = np.abs (selzp - p(selref))
-    cond = (delta < 0.2) & (delta < 2*np.std(delta))
+    cond = (delta < 0.3) & (delta < 3*np.std(delta))
     f = np.polyfit(selref[cond], selzp[cond],1)
     p = np.poly1d(f)
 
-    _logger.info(f"{x} -> {myzp} +/- {f}")
+    _logger.debug(f"{x} -> {myzp} +/- {f[0]}")
 
-    plt.figure()
-    plt.plot(matchedcatlog['refmag'], zp, ".", c='grey')
-    plt.plot(matchedcatlog['refmag'][nbrightest], zp[nbrightest], '.')
-    x = np.arange(10,20)
-    plt.plot (x,p(x))
-    plt.ylim([20, 23])
-    plt.axhline(myzp)
+    if pngname is not None:
+        plt.figure()
+        plt.plot(matchedcatlog['refmag'], zp, ".", c='grey')
+        plt.plot(matchedcatlog['refmag'][nbrightest], zp[nbrightest], '.')
+        x = np.arange(10,20)
+        plt.plot (x,p(x))
+        plt.ylim([myzp-1, myzp+1])
+        plt.axhline(myzp)
+        plt.xlabel ("reference magnitude")
+        plt.ylabel ('reference mag - instrument mag')
 
-    plt.title (f"k = {k:5.3f} z = {z:5.2f}  zp = {myzp:5.2f} ")
-    plt.savefig(f"{datetime.datetime.utcnow()}.png")
-    plt.close()
+        plt.title (f"{pngname}\nk = {k:5.3f} z = {z:5.2f}  zp = {myzp:5.2f} ")
+        plt.savefig(pngname, bbox_inches='tight')
+        plt.close()
 
-    return f[1]
+    return np.abs (f[0])
 
 
 class SingleLinearityFitter():
@@ -99,24 +108,31 @@ class SingleLinearityFitter():
 
     '''
 
-    def __init__(self, imageobject, photcalib=None):
+    def __init__(self, imageobject, photcalib=None, pngstart=None):
         self.imageobject = imageobject
         self.photcalib = PhotCalib('http://phot-catalog.lco.gtn/') if photcalib is None else photcalib
         self.matchedcatalog = self.photcalib.generateCrossmatchedCatalog(imageobject, mintexp=1)
 
-        x0 = [0.0, 1.0]
-        bounds = ((1., 3.), ( 1., 3))
-        #result = scipy.optimize.minimize(fitmerritfunction, x0, (self.imageobject, self.matchedcatalog),
-            #                             bounds=bounds, method='brute')
-        result = scipy.optimize.brute(fitmerritfunction, ((0,5), (1.,2.)), (self.imageobject, self.matchedcatalog),
-                                         )
+        fwhm = self.matchedcatalog['FWHM']
+        print (fwhm)
+
+        x0 = [0.3, 1.05]
+        bounds = ((0., 5.), ( 1., 3.))
+
+        if pngstart is not None:
+            fitmerritfunction((0,1), self.imageobject,self.matchedcatalog,fwhm,pngname=f"{pngstart}_before.png")
+        result = scipy.optimize.minimize(fitmerritfunction, x0, (self.imageobject, self.matchedcatalog,fwhm),
+                                         bounds=bounds, method='SLSQP', options={'eps':0.05})
+        #result = scipy.optimize.brute(fitmerritfunction, ((0,5), (1.,2.)), (self.imageobject, self.matchedcatalog),
+                                         #)
 
         print(f"Fitting result:\n{result}")
-
+        if pngstart is not None:
+            fitmerritfunction(result.x, self.imageobject,self.matchedcatalog,fwhm,pngname=f"{pngstart}_after.png")
 
 def test_fit(image):
     imageobject = fits.open(image)
-    f = SingleLinearityFitter(imageobject)
+    f = SingleLinearityFitter(imageobject, pngstart=os.path.basename(image))
 
 
 
@@ -124,6 +140,9 @@ def test_fit(image):
 logging.basicConfig(level=getattr(logging, 'INFO'),
                     format='%(asctime)s.%(msecs).03d %(levelname)7s: %(module)20s: %(message)s')
 
-test_fit("test/data/tfn0m414-kb95-20211116-0030-e91.fits.fz")
+#test_fit("test/data/tfn0m414-kb95-20211116-0030-e91.fits.fz")
 #test_fit("test/data/tfn0m414-kb95-20211116-0032-e91.fits.fz")
 #test_fit("test/data/tfn0m414-kb95-20211116-0033-e91.fits.fz")
+#test_fit("test/data/cpt0m407-kb87-20211109-0027-e91.fits.fz")
+#test_fit("test/data/lsc0m409-kb96-20191208-0207-e91.fits.fz" )
+test_fit ("test/data/cpt1m012-fa06-20200113-0102-e91.fits.fz")
