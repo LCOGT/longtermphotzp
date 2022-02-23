6d32b949587628f99b4bb84bb42de1d9e650f94e
Daniel Harbeck <drharbeck@gmail.com>
2022-02-23 11:13:57 -0800

    update verrsion number in setup.py to 8.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'longtermphotzp',
-    version="7.1.5",
+    version="8.0.0",
     author='Daniel Harbeck',
     author_email='dharbeck@lco.global',
     classifiers=[
