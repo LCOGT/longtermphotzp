dbfa7fd0571a421c2e32279102ea102a9e6b05ab
Daniel Harbeck <drhaz@users.noreply.github.com>
2023-02-01 16:17:57 -0800

    relase 8.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'longtermphotzp',
-    version="8.1.0",
+    version="8.1.1",
     author='Daniel Harbeck',
     author_email='dharbeck@lco.global',
     classifiers=[
